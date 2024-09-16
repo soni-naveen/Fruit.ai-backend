e8cb90a6af70298bd5e51bde99dd9f10587b13c2
Naveen Soni <naveen0929.be21@chitkara.edu.in>
2024-09-16 08:38:53 +0530

    env issue fixed

--- server.py
+++ server.py
@@ -3,12 +3,14 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": ["https://fruit-ai-frontend-eight.vercel.app"]}})
 
-app.config["MONGO_URI"] = os.getenv('DATABASE_URL')
+app.config["MONGO_URI"] = os.getenv("DATABASE_URL")
 mongo = PyMongo(app)
 
 # function to serialize data from MongoDB
